ffb9cbee9a0abe7bc528e705e311a0a944af1d98
KaioHerculanoLacrei <kaio.herculano@lacreisaude.com.br>
2025-08-27 19:49:24 -0400

    :bug: fix: a bug was fixed when accessing the product detail url in the api

--- products/views.py
+++ products/views.py
@@ -42,11 +42,17 @@ class ProductListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
 
         return list(local_products) + api_products
 
+
 class ProductDetailView(View):
+    api_url = settings.API_URL
+
     def get(self, request, *args, **kwargs):
+        print("Acessando ProductDetailView")
         if 'external_id' in kwargs:
+            print(f"Acessando produto API com external_id: {kwargs['external_id']}")
             return self.get_api_product(request, kwargs['external_id'])
         elif 'pk' in kwargs:
+            print(f"Acessando produto local com pk: {kwargs['pk']}")
             return self.get_local_product(request, kwargs['pk'])
         else:
             messages.error(request, "Produto não encontrado")
@@ -68,12 +74,19 @@ class ProductDetailView(View):
             api_base_url = f"{self.api_url}"
             api_url = f'{api_base_url}/api/v1/public/products/1/{api_id}/'
             
+            print(f"Chamando API: {api_url}")
             response = requests.get(api_url, timeout=5)
+            
+            print(f"Status da resposta da API: {response.status_code}")
+            
             if response.status_code == 404:
                 messages.error(request, "Produto não encontrado na API")
                 return redirect('product_list')
+            
             response.raise_for_status()
             product_data = response.json()
+            
+            print(f"Dados do produto recebidos: {product_data}")
             
             if 'photo' in product_data and product_data['photo']:
                 photo_url_from_api = product_data['photo']
@@ -86,9 +99,15 @@ class ProductDetailView(View):
                 'product': product_data,
                 'is_external': True
             })
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+            messages.error(request, f"Erro ao buscar produto na API: {str(http_err)}")
+            return redirect('product_list')
         except Exception as e:
+            print(f"Erro inesperado: {str(e)}")
             messages.error(request, f"Erro ao buscar produto na API: {str(e)}")
             return redirect('product_list')
+
 
 class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = models.Product
@@ -97,12 +116,14 @@ class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView)
     success_url = reverse_lazy('product_list')
     permission_required = 'products.add_product'
 
+
 class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = models.Product
     form_class = ProductForm
     template_name = 'product_update.html'
     success_url = reverse_lazy('product_list')
     permission_required = 'products.change_product'
+
 
 class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = models.Product
